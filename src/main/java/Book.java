cb0d47d4c2aa7c135b62e05f5b6652a5532f8daa
Anthony Gold and Sam Durfey <anthony@>
2016-05-10 11:50:41 -0700

    pass bookInstantiatesCorrectly test

---
+++ src/main/java/Book.java
@@ -0,0 +1,15 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Book {
+  private int id;
+  private String description;
+
+  public Book(String description) {
+    this.description = description;
+  }
+
+
+
+}
