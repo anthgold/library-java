547f0f02cc22e0d21e10cfed039eed38ce6b475f
Anthony Gold and Sam Durfey <sam@>
2016-05-10 13:16:52 -0700

    pass getTitle

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -4,12 +4,113 @@ import org.sql2o.*;
 
 public class Book {
   private int id;
-  private String description;
+  private String title;
 
-  public Book(String description) {
-    this.description = description;
+  public Book(String title) {
+    this.title = title;
   }
 
+  public String getTitle() {
+    return title;
+  }
 
+/*
+public int getId() {
+  return id;
+}
+
+public static List<Task> all() {
+  String sql = "SELECT id, description FROM tasks";
+  try(Connection con = DB.sql2o.open()) {
+    return con.createQuery(sql).executeAndFetch(Task.class);
+  }
+}
+
+@Override
+public boolean equals(Object otherTask){
+  if (!(otherTask instanceof Task)) {
+    return false;
+  } else {
+    Task newTask = (Task) otherTask;
+    return this.getDescription().equals(newTask.getDescription()) &&
+           this.getId() == newTask.getId();
+  }
+}
+
+public void save() {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO tasks(description) VALUES (:description)";
+    this.id = (int) con.createQuery(sql, true)
+      .addParameter("description", this.description)
+      .executeUpdate()
+      .getKey();
+  }
+}
+
+public static Task find(int id) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "SELECT * FROM tasks where id=:id";
+    Task task = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Task.class);
+    return task;
+  }
+}
+
+public void update(String newDescription) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "UPDATE tasks SET description = :description WHERE id = :id";
+    con.createQuery(sql)
+      .addParameter("description", newDescription)
+      .addParameter("id", this.id) // if buggy /\ from id
+      .executeUpdate();
+  }
+}
+
+public void addCategory(Category category) {
+  try(Connection con = DB.sql2o.open()) {
+    String sql = "INSERT INTO tasks_categories (task_id, category_id) VALUES (:task_id, :category_id)";
+    con.createQuery(sql)
+      .addParameter("task_id", this.getId())
+      .addParameter("category_id", category.getId())
+      .executeUpdate();
+  }
+}
+
+public List<Category> getCategories() {
+  try(Connection con = DB.sql2o.open()) {
+    String joinQuery = "SELECT category_id FROM tasks_categories WHERE task_id = :task_id";
+    List<Integer> categoryIds = con.createQuery(joinQuery)
+      .addParameter("task_id", this.getId())
+      .executeAndFetch(Integer.class);
+
+    List<Category> categories = new ArrayList<Category>();
+
+    for (Integer categoryId : categoryIds) {
+      String taskQuery = "SELECT * FROM categories WHERE id = :categoryId";
+      Category category = con.createQuery(taskQuery)
+        .addParameter("categoryId", categoryId)
+        .executeAndFetchFirst(Category.class);
+      categories.add(category);
+    }
+    return categories;
+  }
+}
+
+public void delete() {
+  try(Connection con = DB.sql2o.open()) {
+    String deleteQuery = "DELETE FROM tasks WHERE id = :id;";
+      con.createQuery(deleteQuery)
+        .addParameter("id", this.getId())
+        .executeUpdate();
+
+    String joinDeleteQuery = "DELETE FROM tasks_categories WHERE task_id = :taskId";
+      con.createQuery(joinDeleteQuery)
+        .addParameter("taskId", this.getId())
+        .executeUpdate();
+    }
+  }
+
+*/
 
 }
--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -18,4 +18,94 @@ public class BookTest {
     assertEquals("A Tale of Two Cities", myBook.getTitle());
   }
 
+/*
+
+@Test
+public void all_emptyAtFirst_0() {
+  assertEquals(0, Task.all().size());
+}
+
+@Test
+public void equals_returnsTrueIfDescriptionsAretheSame_true() {
+  Task firstTask = new Task("Mow the lawn");
+  Task secondTask = new Task("Mow the lawn");
+  assertTrue(firstTask.equals(secondTask));
+}
+
+@Test
+public void save_savesObjectIntoDB_true() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  assertTrue(Task.all().get(0).equals(myTask));
+}
+
+@Test
+public void save_assignsIdToObject_int() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  Task savedTask = Task.all().get(0);
+  assertEquals(myTask.getId(), savedTask.getId());
+}
+
+@Test
+public void find_findsTaskInDatabase_true() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  Task savedTask = Task.find(myTask.getId());
+  assertTrue(myTask.equals(savedTask));
+}
+
+
+@Test
+public void update_updatesTaskDescription_true() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  myTask.update("Take a nap");
+  assertEquals("Take a nap", Task.find(myTask.getId()).getDescription());
+}
+
+@Test
+public void delete_deletesTask_true() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  int myTaskId = myTask.getId();
+  myTask.delete();
+  assertEquals(null, Task.find(myTaskId));
+}
+
+@Test
+public void addCategory_addsCategoryToTask_true() {
+    Category myCategory = new Category("Shopping");
+    myCategory.save();
+    Task myTask = new Task("Buy butter");
+    myTask.save();
+    myTask.addCategory(myCategory);
+    Category savedCategory = myTask.getCategories().get(0);
+    assertTrue(myCategory.equals(savedCategory));
+}
+
+@Test
+public void getCategories_returnsAllCategories_List() {
+  Category myCategory = new Category("Maintenance");
+  myCategory.save();
+  Task myTask = new Task("Change the oil");
+  myTask.save();
+  myTask.addCategory(myCategory);
+  List savedCategories = myTask.getCategories();
+  assertEquals(1, savedCategories.size());
+}
+
+@Test
+public void delete_deletesAllTasksAndCategoriesAssociations_true() {
+  Category myCategory = new Category("Dental procedures");
+  myCategory.save();
+  Task myTask = new Task("Polish molars");
+  myTask.save();
+  myTask.addCategory(myCategory);
+  myTask.delete();
+  assertEquals(0, myCategory.getTasks().size());
+}
+
+*/
+
 }
