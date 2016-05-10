f38b6d561abc1d5868e6332b628ee9ae4f78ba56
Anthony Gold and Sam Durfey <anthony@>
2016-05-10 14:01:39 -0700

    Add root page, rootTest will pass.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -19,9 +19,9 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("Task list!");
-  // }
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Welcome to the Library!");
+  }
 }
